5bed7161b113a1d6596e0d78e38eda519ff98589
Zoe Albers <146694623+Zoobaru820@users.noreply.github.com>
2025-11-12 08:53:20 -0600

    Update addCourse.php

--- addCourse.php
+++ addCourse.php
@@ -1,76 +1,40 @@
 <?php
-session_start();
 require_once("WebServiceClient.php");
 require_once(__DIR__ . "/../creds.php");
 
-
-$_SESSION['errors'] = array();
-$_SESSION['loggedIn'] = false;
-
-if (!isset($_POST['username']) || empty($_POST['username'])) {
-    $_SESSION['errors'] [] = "Please fill in the username";
-}
-if (!isset($_POST['password']) || empty($_POST['password'])) {
-    $_SESSION['errors'] [] = "Please fill in the password";
-}
-
-// If we have validation errors, return to login page
-if (count($_SESSION['errors']) > 0) {
-    header("Location: login.php");
-    exit();
-}
-
-$apikey = APIKEY;
-$apihash = APIHASH;
-
-// Call the authentication web service with the user's submitted credentials
+// Create a new instance of WebServiceClient
 $url = "https://cnmt310.classconvo.com/classreg/";
 $client = new WebServiceClient($url);
 
-$data = array(
-    "username" => isset($_POST['username']) ? trim($_POST['username']) : '',
-    "password" => isset($_POST['password']) ? trim($_POST['password']) : ''
-);
+// Use your API key and hash from creds.php
+$apikey = APIKEY;
+$apihash = APIHASH;
 
+// Build the JSON payload to add a course
 $fields = array(
-    "apikey" => $apikey,
-    "apihash" => $apihash,
-    "data" => $data,
-    "action" => "authenticate",
+    "apikey" => $api52,
+    "apihash" => $oRWS35HrhE,
+    "action" => "addcourse",
+    "data" => array(
+        "coursename" => "Principles of Computing",
+        "coursecode" => "CNMT",
+        "coursenum" => "100",
+        "coursecredits" => "3",
+        "coursedesc" => "Exploring the principles of computing",
+        "courseinstr" => "Simkins",
+        "meetingtimes" => "MW 11:00a-12:15p",
+        "maxenroll" => "24"
+    )
 );
 
+// Set the fields to send as JSON
 $client->setPostFields($fields);
 
+// Send the request
 $result = $client->send();
+
+// Decode the JSON result
 $json = json_decode($result);
 
-// Check if the response is valid JSON
-if (json_last_error() !== JSON_ERROR_NONE) {
-    $_SESSION['errors'][] = "Authentication service error. Please try again later.";
-    header("Location: login.php");
-    exit();
-}
-
-// Check authentication result
-$authenticated = false;
-
+// Display (dump) the response
 var_dump($json);
-
-if (isset($json->result) && $json->result == "Success") {
-    $authenticated = true;
-}
-
-if ($authenticated) {
-    $_SESSION['loggedIn'] = true;
-    $_SESSION['username'] = $_POST['username'];
-    $_SESSION['baseRole'] = $json->data->user_role;
-    if (isset($json->data) && is_object($json->data)) {
-        $_SESSION['user'] = (array)$json->data;
-    }
-    header("Location: account.php");
-    exit();
-} else {
-    $_SESSION['errors'][] = "Invalid username or password";
-    header("Location: login.php");
-    exit();
-}
